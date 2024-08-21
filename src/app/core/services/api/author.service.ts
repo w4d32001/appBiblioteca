c3b5337bf1ede333675d566175aebdbd235eb6a0
Kevin <kinn.n1998ags@gmail.com>
2024-08-20 22:11:17 -0500

    autor

--- src/app/core/services/api/author.service.ts
+++ src/app/core/services/api/author.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_URL } from '../base/API_URL';
+import { HttpClient } from '@angular/common/http';
+import { Author, AuthorResponse } from '../../models/author';
+import { BaseService } from '../generic/base.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthorService {
+export class AuthorService extends BaseService{
 
-  constructor() { }
+  private url = API_URL + 'author';
+  private http = inject(HttpClient);
+
+  protected getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+  getAthors(): Observable<AuthorResponse> {
+    return this.http.get<AuthorResponse>(this.url, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+  getAuthor(id: number): Observable<AuthorResponse> {
+    return this.http.get<AuthorResponse>(${this.url}/${id}, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+
+  createAuthor(author: Author): Observable<{ message: string }> {
+    return this.http.post<{ message: string }>(this.url, author, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+
+  updateAuthor(
+    id: number,
+    author: Author
+  ): Observable<{ message: string }> {
+    return this.http.put<{ message: string }>(${this.url}/${id}, author, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+
+  deleteAuhtor(id: number): Observable<{ message: string }> {
+    return this.http.delete<{ message: string }>(${this.url}/${id}, {
+      headers: this.getAuthHeaders(),
+    });
+  }
 }
