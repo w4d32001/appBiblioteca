f40a53011445155cb80b0233e8a55a40e781821d
jeferssonme <merinojefersson@gmail.com>
2024-08-20 20:28:54 -0500

    servicios de libro y categoria

--- src/app/core/services/api/book.service.ts
+++ src/app/core/services/api/book.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { API_URL } from '../base/API_URL';
+import { Observable } from 'rxjs';
+import { Book, BookResponse } from '../../models/book';
+import { BaseService } from '../generic/base.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BookService {
+export class BookService extends BaseService {
 
-  constructor() { }
+  private url = API_URL + 'book';
+  private http = inject(HttpClient);
+
+  protected getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+  getBooks(): Observable<BookResponse> {
+    return this.http.get<BookResponse>(this.url, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+  getBook(id: number): Observable<BookResponse> {
+    return this.http.get<BookResponse>(${this.url}/${id}, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+
+  createBook(book: Book): Observable<{ message: string }> {
+    return this.http.post<{ message: string }>(this.url, book, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+
+  updateBook(
+    id: number,
+    book: Book
+  ): Observable<{ message: string }> {
+    return this.http.put<{ message: string }>(${this.url}/${id}, book, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+
+  deleteBook (id: number): Observable<{ message: string }> {
+    return this.http.delete<{ message: string }>(${this.url}/${id}, {
+      headers: this.getAuthHeaders(),
+    });
+  }
 }
--- src/app/core/services/api/category.service.ts
+++ src/app/core/services/api/category.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { API_URL } from '../base/API_URL';
+import { HttpClient } from '@angular/common/http';
+import { Category, CategoryResponse } from '../../models/category';
+import { Observable } from 'rxjs';
+import { BaseService } from '../generic/base.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CategoryService {
+export class CategoryService extends BaseService {
+  private url = API_URL + 'category';
+  private http = inject(HttpClient);
 
-  constructor() { }
+  protected getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+  getCategories(): Observable<CategoryResponse> {
+    return this.http.get<CategoryResponse>(this.url, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+  getCategory(id: number): Observable<CategoryResponse> {
+    return this.http.get<CategoryResponse>(${this.url}/${id}, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+
+  createCategory(category: Category): Observable<{ message: string }> {
+    return this.http.post<{ message: string }>(this.url, category, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+
+  updateCategory(
+    id: number,
+    category: Category
+  ): Observable<{ message: string }> {
+    return this.http.put<{ message: string }>(${this.url}/${id}, category, {
+      headers: this.getAuthHeaders(),
+    });
+  }
+
+  deleteCategory(id: number): Observable<{ message: string }> {
+    return this.http.delete<{ message: string }>(${this.url}/${id}, {
+      headers: this.getAuthHeaders(),
+    });
+  }
 }
